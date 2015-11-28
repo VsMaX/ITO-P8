3020371300268a6711ea164c56b0be7927c56278
Michal Cwienczek <michael.cwienczek@gmail.com>
2015-11-28 20:17:23 +0100

    Work in progress

---
+++ ITO-Graph/Dfs.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITO_Graph
+{
+    public class Dfs
+    {
+        public Stack<Node> TraverseNodes { get; set; } 
+
+        public void Traverse()
+        {
+            while (TraverseNodes.Any())
+            {
+                Node node = TraverseNodes.Pop();
+                foreach (var action in AvailableActions)
+                {
+                    if (action.FulFillsPreconditions(node.EffectsBefore))
+                    {
+                        TraverseNodes.Push();
+                    }
+                }
+            }
+        }
+
+        public IEnumerable<StripAction> AvailableActions { get; set; }
+    }
+}
